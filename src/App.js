67dc51fb083568dbfffbcc9025434f69f0d4fea0
adam-nearmap <adam.ziman@nearmap.com>
2019-05-13 18:03:12 +1000

    Remove material UI components and update styles

--- src/App.js
+++ src/App.js
@@ -1,48 +1,32 @@
 import React, { useState } from 'react';
-import PropTypes from 'prop-types';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from "react-apollo";
 import debounce from './helper';
-import TextField from '@material-ui/core/TextField';
-import { withStyles } from '@material-ui/core/styles';
 import MovieList from './list';
 import './App.css';
-
-const styles = theme => ({
-  textField: {
-    marginLeft: theme.spacing.unit,
-    marginRight: theme.spacing.unit,
-    width: 200,
-  }
-});
 
 const client = new ApolloClient({
   uri: 'https://112qaej5y9.execute-api.ap-southeast-2.amazonaws.com/dev/graphql'
 });
 
-const App = (props)=> {
-  const { classes } = props;
+const App = ()=> {
   const [state, setState] = useState({actor: 'tom cruise'});
 
   const setName = debounce((value) => {
     setState({ actor: value })
-  }, 1000)
+  }, 100)
 
   return (
     <ApolloProvider client={client}>
-      <div className="App">
-        <header className="App-header">
+      <div className='App'>
+        <header className='App-header'>
           <h1>Tom Cruise movie night</h1>
         </header>
         <main>
-          <p>Type in the name of another actor you are looking for</p>
-          <TextField
-            id="actor-name"
-            label="Actor"
-            className={classes.textField}
-            onChange={(event)=> setName(event.target.value)}
-            margin="normal"
-          />
+          <p>
+            <label>Search for movies by actor</label>
+            <input type='text' name='actor' onChange={(event)=> setName(event.target.value)}/>
+          </p>
           <MovieList actor={state.actor} />
         </main>
       </div>
@@ -50,8 +34,4 @@ const App = (props)=> {
   );
 }
 
-App.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
-
-export default withStyles(styles)(App);
+export default App;
--- src/list/index.js
+++ src/list/index.js
@@ -1,37 +1,12 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { Query } from "react-apollo";
 import gql from "graphql-tag";
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
-import ListItemAvatar from '@material-ui/core/ListItemAvatar';
-import Avatar from '@material-ui/core/Avatar';
-import { withStyles } from '@material-ui/core/styles';
 import noImage from './image-not-found.png';
+import './theme.css';
 
-const styles = theme => ({
-  root: {
-    width: '100%',
-    backgroundColor: theme.palette.background.paper,
-  },
-  inline: {
-    display: 'inline',
-  },
-  poster: {
-    width: 100,
-    height: 'auto',
-    'border-radius': 0
-  },
-  textField: {
-    marginLeft: theme.spacing.unit,
-    marginRight: theme.spacing.unit,
-    width: 200,
-  }
-});
 
 const MovieList = (props) => {
-  const { classes, actor } = props;
+  const { actor } = props;
   
   return (<Query
       query={gql`
@@ -58,25 +33,22 @@ const MovieList = (props) => {
         const movies = data.searchPeople[0].appearsIn;
 
         return (
-            <List className={classes.root}>
+            <ul>
               {movies.map(({ id, title, overview, poster }) => (
-                <ListItem key={id} alignItems='flex-start'>
-                  <ListItemAvatar>
-                    <Avatar className={classes.poster} alt={title} src={poster ? poster.thumbnail : noImage} />
-                  </ListItemAvatar>
-                  <ListItemText
-                    primary={title}
-                    secondary={overview}
-                  />
-                </ListItem>
+                <li key={id} alignItems='flex-start'>
+                  <span className='poster'>
+                    <img alt={title} src={poster ? poster.thumbnail : noImage} />
+                  </span>
+                  <span className='text'>
+                    <p><strong>{title}</strong></p>
+                    <p>{overview}</p>
+                  </span>
+                </li>
               ))}
-            </List>
+            </ul>
         );
       }}
     </Query>);
 };
 
-MovieList.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
-export default withStyles(styles)(MovieList);
+export default MovieList;
